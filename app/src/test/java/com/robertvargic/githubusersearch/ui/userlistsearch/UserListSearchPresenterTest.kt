d4f77a271d571e075ded2cc3017d2bf6241d5046
Robert Vargic <robert.vargic@gmail.com>
2018-06-12 12:23:24 +0200

    Test location and naming fixed

--- app/src/test/java/com/robertvargic/githubusersearch/ui/userlistsearch/UserListSearchPresenterTest.kt
+++ app/src/test/java/com/robertvargic/githubusersearch/ui/userlistsearch/UserListSearchPresenterTest.kt
@@ -11,7 +11,7 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
-class UserSearchPresenterTest {
+class UserListSearchPresenterTest {
     lateinit var presenter: UserListSearchPresenter
 
     @Mock
