4c4c05f40ffb32601c8c454a043f15856d0cf408
Robert Vargic <robert.vargic@gmail.com>
2018-06-14 11:23:12 +0200

    Fixed bottom navigation with viewpager implementation

--- app/src/main/java/com/robertvargic/githubusersearch/ui/bottomnavigation/BottomNavigationBarActivity.kt
+++ app/src/main/java/com/robertvargic/githubusersearch/ui/bottomnavigation/BottomNavigationBarActivity.kt
@@ -3,6 +3,7 @@ package com.robertvargic.githubusersearch.ui.bottomnavigation
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.ViewPager
+import android.view.MenuItem
 import com.robertvargic.githubusersearch.R
 import com.robertvargic.githubusersearch.ui.adapters.ViewPagerAdapter
 import com.robertvargic.githubusersearch.ui.base.BaseActivity
@@ -17,6 +18,7 @@ class BottomNavigationBarActivity : BaseActivity() {
     var searchFragment = UserListSearchFragment()
     var favouriteFragment = FavouriteUserListFragment()
     var deleteFragment = DeleteDatabaseFragment()
+    var prevMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +34,9 @@ class BottomNavigationBarActivity : BaseActivity() {
         viewPagerAdapter.addFragment(deleteFragment, Constants.DELETE_DATABASE_FRAGMENT)
 
         viewPager.adapter = viewPagerAdapter
+
+        viewPager.addOnPageChangeListener(onPageChangeListener)
+
     }
 
     private val navigationListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -50,5 +55,23 @@ class BottomNavigationBarActivity : BaseActivity() {
             }
         }
         false
+    }
+
+    private val onPageChangeListener = object: ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+        }
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        }
+
+        override fun onPageSelected(position: Int) {
+            if (prevMenuItem != null) {
+                prevMenuItem!!.isChecked = false
+            } else {
+                navigation.menu.getItem(0).isChecked = false
+            }
+            navigation.menu.getItem(position).isChecked = true
+            prevMenuItem = navigation.menu.getItem(position)
+        }
     }
 }
